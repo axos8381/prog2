8fa080e5a48aefbc6552361ed9055d48001e3f64
axos8381 <115161796+axos8381@users.noreply.github.com>
2022-10-07 19:10:09 +0200

    added numba function

--- MA4_2.py
+++ MA4_2.py
@@ -2,6 +2,8 @@
 
 from person import Person
 import time
+from numba import njit
+
 
 def fib_py(n):
 	if n <= 1:
@@ -9,8 +11,14 @@ def fib_py(n):
 	else:
 		return(fib_py(n-1) + fib_py(n-2))
 
+
+@njit
 def  fib_numba(n):
-	pass
+	if n <= 1:
+		return n
+	else:
+		return(fib_py(n-1) + fib_py(n-2))
+	
 
 def main():
 	timeFibC = []
@@ -25,7 +33,16 @@ def main():
 			tstopC = time.perf_counter()
 			tidC = tstopC - tstartC
 			timeFibC.append(tidC)
-		#if x <= 35:
+		else:
+			timeFibC.append(0)
+
+
+		tstartN = time.perf_counter()
+		fib_numba(x)
+		tstopN = time.perf_counter()
+		tidN = tstopN - tstartN
+		timeFibP.append(tidN)
+
 		tstartP = time.perf_counter()
 		fib_py(x)
 		tstopP = time.perf_counter()
@@ -39,18 +56,18 @@ def main():
 
 	f = Person(47)
 	tstartC = time.perf_counter()
-	value = f.fib()
+	f.fib()
 	tstopC = time.perf_counter()
 	tidC = tstopC - tstartC
-	print(f"fib for Person(47) is {value} and took {tidC} seconds to calculate using C++ function")
+	print(f"fib for Person(47) took {tidC} seconds to calculate using C++ function")
 
-	
-	
-	# f = Person(5)
-	# print(f.get())
-	# f.set(7)
-	# print(f"kan man få en fibbe eller?: {f.fib()}")
-	# print(f.get())
+	tstartN = time.perf_counter()
+	fib_numba(47)
+	tstopN = time.perf_counter()
+	tidN = tstopN - tstartN
+	print(f"fib for fib(47) took {tidN} seconds to calculate using Python with numba function")
+
+
 	
 
 if __name__ == '__main__':
