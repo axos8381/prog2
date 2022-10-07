ea6dacd9725b224a541a7eb4d6b9976af7750213
axos8381 <115161796+axos8381@users.noreply.github.com>
2022-10-07 17:38:49 +0200

    for loop to test fib in C++ time, n=30-45

--- MA4_2.py
+++ MA4_2.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python3.9
 
 from person import Person
+import time
+
+def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return(fib_py(n-1) + fib_py(n-2))
+
+def  fib_numba(n):
+	pass
 
 def main():
-	f = Person(5)
-	print(f.get())
-	f.set(7)
-	print(f"kan man få en fibbe eller?: {f.fib()}")
-	print(f.get())
+	timeFibC = []
+	timeFibP = []
+	timeFibN = []
+	test1 = list(range(30, 45))
+	for x in test1:
+		f = Person(x)
+		tstartC = time.perf_counter()
+		f.fib()
+		tstopC = time.perf_counter()
+		tidC = tstartC - tstopC
+		timeFibC.append(tidC)
+
+	print(timeFibC)
+
+
+	
+	
+	# f = Person(5)
+	# print(f.get())
+	# f.set(7)
+	# print(f"kan man få en fibbe eller?: {f.fib()}")
+	# print(f.get())
 	
 
 if __name__ == '__main__':
