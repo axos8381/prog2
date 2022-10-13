78b74c05b36a860badcabd208152ac7c815d3867
axos8381 <115161796+axos8381@users.noreply.github.com>
2022-10-13 16:15:04 +0200

    chagned to final state

--- MA4_2.py
+++ MA4_2.py
@@ -21,29 +21,6 @@ def  fib_numba(n):
 		return(fib_numba(n-1) + fib_numba(n-2))
 
 
-
-#uppgift 1 funktion:
-
-def monteCarlo(n,r):
-	xcordInside = []
-	ycordInside = []
-	xcordOutside = []
-	ycordOutside = []
-
-	for i in range(n):
-		y = random.uniform(-1*r,1*r)
-		x = random.uniform(-1*r,1*r)
-		absLen = math.sqrt( pow(x,2) + pow(y,2))
-
-		if absLen > r:
-			xcordOutside.append(x)
-			ycordOutside.append(y)
-
-		else:
-			xcordInside.append(x)
-			ycordInside.append(y)
-
-	return xcordInside, ycordInside, xcordOutside, ycordOutside
 	
 
 def main():
@@ -69,26 +46,26 @@ def main():
 		tstopN = time.perf_counter()
 		tidN = tstopN - tstartN
 		timeFibN1.append(tidN)
-	# 	if x < 44:
-	# 		tstartP = time.perf_counter()
-	# 		fib_py(x)
-	# 		tstopP = time.perf_counter()
-	# 		tidP = tstopP - tstartP
-	# 		timeFibP1.append(tidP)
+		if x < 44:
+			tstartP = time.perf_counter()
+			fib_py(x)
+			tstopP = time.perf_counter()
+			tidP = tstopP - tstartP
+			timeFibP1.append(tidP)
 		
 		
-	# for x in nPlot2:
-	# 	tstartN = time.perf_counter()
-	# 	fib_numba(x)
-	# 	tstopN = time.perf_counter()
-	# 	tidN = tstopN - tstartN
-	# 	timeFibN2.append(tidN)
+	for x in nPlot2:
+		tstartN = time.perf_counter()
+		fib_numba(x)
+		tstopN = time.perf_counter()
+		tidN = tstopN - tstartN
+		timeFibN2.append(tidN)
 
-	# 	tstartP = time.perf_counter()
-	# 	fib_py(x)
-	# 	tstopP = time.perf_counter()
-	# 	tidP = tstopP - tstartP
-	# 	timeFibP2.append(tidP)
+		tstartP = time.perf_counter()
+		fib_py(x)
+		tstopP = time.perf_counter()
+		tidP = tstopP - tstartP
+		timeFibP2.append(tidP)
 
 	
 
@@ -99,55 +76,42 @@ def main():
 		
 		
 
-	# f = Person(47)
-	# tstartC = time.perf_counter()
-	# f.fib()
-	# tstopC = time.perf_counter()
-	# tidC = tstopC - tstartC
-	# print(f"fib for Person(47) took {tidC} seconds to calculate using C++ function")
+	f = Person(47)
+	tstartC = time.perf_counter()
+	f.fib()
+	tstopC = time.perf_counter()
+	tidC = tstopC - tstartC
+	print(f"fib for Person(47) took {tidC} seconds to calculate using C++ function")
 
-	# tstartN = time.perf_counter()
-	# fib_numba(47)
-	# tstopN = time.perf_counter()
-	# tidN = tstopN - tstartN
-	# print(f"fib for fib(47) took {tidN} seconds to calculate using Python with numba function")
+	tstartN = time.perf_counter()
+	fib_numba(47)
+	tstopN = time.perf_counter()
+	tidN = tstopN - tstartN
+	print(f"fib for fib(47) took {tidN} seconds to calculate using Python with numba function")
 
 
-
-	# plt.figure(figsize=(5, 2.7), layout='constrained')
-	# plt.plot(nPlot1, timeFibN1, label='Numba Python')
-	# plt.plot(nPlot1, timeFibC, label='C++')
-	# plt.plot(nPlotP1, timeFibP1, label='Normal Python')
-	
-	# plt.xlabel('n')
-	# plt.ylabel('Time [s]')
-	# plt.title("Fibonacci calculations")
-	# plt.legend()
-	# plt.savefig('Fibonacci_plot_high_n.png')
 
 	plt.figure(figsize=(5, 2.7), layout='constrained')
 	plt.plot(nPlot1, timeFibN1, label='Numba Python')
 	plt.plot(nPlot1, timeFibC, label='C++')
+	plt.plot(nPlotP1, timeFibP1, label='Normal Python')
 	
 	plt.xlabel('n')
 	plt.ylabel('Time [s]')
 	plt.title("Fibonacci calculations")
 	plt.legend()
-	plt.savefig('Fibonacci_plot_numba_c.png')
-
-
-	# plt.figure(figsize=(5, 2.7), layout='constrained')
-	# plt.plot(nPlot2, timeFibP2, label='Normal Python')
-	# plt.plot(nPlot2, timeFibN2, label='Numba Python')
-	# plt.xlabel('n')
-	# plt.ylabel('Time [s]')
-	# plt.title("Fibonacci calculations")
-	# plt.legend()
-	# plt.savefig('Fibonacci_plot_low_n.png')
+	plt.savefig('Fibonacci_plot_high_n.png')
 
 
 
-	#plt.savefig('Fibonacci_plot.png')
+	plt.figure(figsize=(5, 2.7), layout='constrained')
+	plt.plot(nPlot2, timeFibP2, label='Normal Python')
+	plt.plot(nPlot2, timeFibN2, label='Numba Python')
+	plt.xlabel('n')
+	plt.ylabel('Time [s]')
+	plt.title("Fibonacci calculations")
+	plt.legend()
+	plt.savefig('Fibonacci_plot_low_n.png')
 
 
 
@@ -157,39 +121,8 @@ def main():
 
 
 
-	#Upgift 1:
 
-	
 
-	nrOfPoints = 1000
-	xListIn, yListIn, xListOut, yListOut = monteCarlo(nrOfPoints, 1)
-
-	fig, ax=plt.subplots(1)
-	ax.set_aspect('equal')
-	ax.scatter(xListIn, yListIn, c='r', alpha=0.8, edgecolors=None)
-	ax.scatter(xListOut, yListOut, c='b', alpha=0.8, edgecolors=None)
-	ax.set_title(f"plot with n = {nrOfPoints}")
-	fig.savefig('pi_1000_points.png')
-
-	nrOfPoints = 10000
-	xListIn, yListIn, xListOut, yListOut = monteCarlo(nrOfPoints, 1)
-
-	fig, ax=plt.subplots(1)
-	ax.set_aspect('equal')
-	ax.scatter(xListIn, yListIn, c='r', alpha=0.8, edgecolors=None)
-	ax.scatter(xListOut, yListOut, c='b', alpha=0.8, edgecolors=None)
-	ax.set_title(f"plot with n = {nrOfPoints}")
-	fig.savefig('pi_10000_points.png')
-
-	nrOfPoints = 100000
-	xListIn, yListIn, xListOut, yListOut = monteCarlo(nrOfPoints, 1)
-
-	fig, ax=plt.subplots(1)
-	ax.set_aspect('equal')
-	ax.scatter(xListIn, yListIn, c='r', alpha=0.8, edgecolors=None)
-	ax.scatter(xListOut, yListOut, c='b', alpha=0.8, edgecolors=None)
-	ax.set_title(f"plot with n = {nrOfPoints}")
-	fig.savefig('pi_100000_points.png')
 
 
 
